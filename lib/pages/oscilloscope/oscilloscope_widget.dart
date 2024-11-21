0795e1910676251d812346ef62e5eeb8585e1e91
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-11-21 06:04:12 +0000

    Osc chart timing...

--- lib/pages/oscilloscope/oscilloscope_widget.dart
+++ lib/pages/oscilloscope/oscilloscope_widget.dart
@@ -154,7 +154,7 @@ class _OscilloscopeWidgetState extends State<OscilloscopeWidget> {
                                     'Oscilloscope',
                                   );
                                   _model.instantTimer = InstantTimer.periodic(
-                                    duration: Duration(milliseconds: 500),
+                                    duration: Duration(milliseconds: 250),
                                     callback: (timer) async {
                                       _model.dataaa = await actions.receiveData(
                                         widget!.devicee!,
